ae1154603690eba91b2c01a96a3ce9a5414c8650
Ulan Jumabaev <ulan.five@gmail.com>
2018-08-06 19:38:38 +0600

    new baza

--- db_reserves.sql
+++ db_reserves.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1
--- Время создания: Авг 06 2018 г., 12:14
+-- Время создания: Авг 06 2018 г., 15:36
 -- Версия сервера: 10.1.31-MariaDB
 -- Версия PHP: 7.0.29
 
@@ -66,21 +66,72 @@ CREATE TABLE `category_two` (
 --
 
 INSERT INTO `category_two` (`id`, `title`, `description`, `image_name`) VALUES
-(15, '\'primer 1\'', '\'primer 1primer 1primer 1primer 1primer 1primer 1primer 1primer 1primer 1primer 1primer 1primer 1primer 1primer 1primer 1primer 1primer 1primer 1\'', '\'dc0964eec98ecdfe625e8ce93e579939.png\''),
-(16, '\'primer 2\'', '\'primer 2primer 2primer 2primer 2primer 2primer 2primer 2primer 2primer 2primer 2primer 2primer 2primer 2primer 2primer 2primer 2\'', '\'094f6a3a42e0b849d4928464021b52fa.jpg\'');
+(24, 'service', 'new', 'e8e80116e26e439bf4f288017e4ec4fb.jpg');
 
 -- --------------------------------------------------------
 
 --
--- Структура таблицы `services`
+-- Структура таблицы `services_book`
 --
 
-CREATE TABLE `services` (
+CREATE TABLE `services_book` (
   `id` int(11) NOT NULL,
   `title` text NOT NULL,
   `description` text NOT NULL,
-  `img_name` int(11) NOT NULL,
-  `action` varchar(255) NOT NULL
+  `img_name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `services_restaurants`
+--
+
+CREATE TABLE `services_restaurants` (
+  `id` int(11) NOT NULL,
+  `title` text NOT NULL,
+  `description` text NOT NULL,
+  `img_name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `services_tickets_show`
+--
+
+CREATE TABLE `services_tickets_show` (
+  `id` int(11) NOT NULL,
+  `title` text NOT NULL,
+  `description` text NOT NULL,
+  `img_name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `services_transports`
+--
+
+CREATE TABLE `services_transports` (
+  `id` int(11) NOT NULL,
+  `title` text NOT NULL,
+  `description` text NOT NULL,
+  `img_name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `sub_category_two`
+--
+
+CREATE TABLE `sub_category_two` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `img_name` varchar(255) NOT NULL,
+  `category_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -106,9 +157,33 @@ ALTER TABLE `category_two`
   ADD PRIMARY KEY (`id`);
 
 --
--- Индексы таблицы `services`
+-- Индексы таблицы `services_book`
 --
-ALTER TABLE `services`
+ALTER TABLE `services_book`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `services_restaurants`
+--
+ALTER TABLE `services_restaurants`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `services_tickets_show`
+--
+ALTER TABLE `services_tickets_show`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `services_transports`
+--
+ALTER TABLE `services_transports`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `sub_category_two`
+--
+ALTER TABLE `sub_category_two`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -131,12 +206,36 @@ ALTER TABLE `category_three`
 -- AUTO_INCREMENT для таблицы `category_two`
 --
 ALTER TABLE `category_two`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
--- AUTO_INCREMENT для таблицы `services`
+-- AUTO_INCREMENT для таблицы `services_book`
 --
-ALTER TABLE `services`
+ALTER TABLE `services_book`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `services_restaurants`
+--
+ALTER TABLE `services_restaurants`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `services_tickets_show`
+--
+ALTER TABLE `services_tickets_show`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `services_transports`
+--
+ALTER TABLE `services_transports`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `sub_category_two`
+--
+ALTER TABLE `sub_category_two`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
