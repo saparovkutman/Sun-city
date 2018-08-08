e560c5bff70e3f81cda2dc520353e0943d0582bc
Ulan Jumabaev <ulan.five@gmail.com>
2018-08-08 14:26:02 +0600

    2

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://php2.kg/';
+$config['base_url'] = 'http://localhost/sun-city';
 /*
 |--------------------------------------------------------------------------
 | Index File
